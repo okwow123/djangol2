5ced4b9419561106a8e6a50cf1a04524c43aa12d
mj <okwow123@naver.com>
2017-05-22 20:46:49 +0900

    complete search

--- example/example/views.py
+++ example/example/views.py
@@ -4,11 +4,18 @@ from django.shortcuts import render
 from django.contrib.auth.models import User
 from registration.forms import RegistrationForm
 from django.views.decorators.csrf import csrf_exempt
-from .models import Store
+#from .models import Store
+#from django.db import models
+from store.models import *
 
 @csrf_exempt    
 def main(request):
-    #store=Store.objects.all()
-    #return render(request,'store/list.html',{'store':store})
+    if request.POST.get('storesearch')==None:
+        store=Store.objects.all()
+    else:
+        store=Store.objects.filter(store_name=request.POST.get('storesearch'))
 
-    return render(request,'index.html',{})
+    return render(request,'index.html',{'store':store})
+
+    
+    #return render(request,'index.html',{})
